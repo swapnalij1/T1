e74c994be9199da16fa91e8ec3e29c8a6cf3d26f
swapnalij1 <swapnalij4050@gmail.com>
2020-12-13 13:55:50 +0530

    R1

---
+++ src/Tc1.java
@@ -0,0 +1,11 @@
+
+public class Tc1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("a");
+		System.out.println("b");
+		System.out.println("c");
+	}
+
+}
